2d975925f50c733bd01aa70157cb751f3d527618
Nik <nikblvck@icloud.com>
2021-12-03 15:56:04 -0800

    nav/login/signup

---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
+
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
+
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = <ProfileButton user={sessionUser} />;
+  } else {
+    sessionLinks = (
+      <>
+        <NavLink className="rNavLinks" to="/login">Log In</NavLink>
+        <NavLink className="rNavLinks" to="/signup">Sign Up</NavLink>
+      </>
+    );
+  }
+
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+      </li>
+
+      <li className="navRight">{sessionLinks}</li>
+    </ul>
+  );
+}
+
+export default Navigation;
