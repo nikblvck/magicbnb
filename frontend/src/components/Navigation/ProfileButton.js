26cfaaaf05e646288c675d5c1b4825ca085d8309
Nik <nikblvck@icloud.com>
2021-12-13 09:06:23 -0800

    fixed broken spots page

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -30,7 +30,7 @@ function ProfileButton({ user }) {
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout())
-    history.push('/spots');
+    history.push('/');
   };
 
   const handleClickToCreate = (e) => {
