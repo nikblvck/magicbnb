0df205d3a282a539838d62338b7e0bf899ba5ecd
Nik <nikblvck@icloud.com>
2021-12-13 08:46:47 -0800

    update

--- frontend/src/components/ReviewsDiv/index.js
+++ frontend/src/components/ReviewsDiv/index.js
@@ -1,21 +1,16 @@
-import {useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import "./ReviewsDiv.css"
+import "./ReviewsDiv.css";
 
-function ReviewsDiv () {
- const {spotId} = useParams();
- const dispatch = useDispatch();
- const reviews = useSelector((state) => state.spots[spotId]?.Reviews)
- const sessionUser = useSelector((state=> state.session.user?.id));
- const review = useSelector(state => state?.spots[spotId]?.Review)
- const handleSubmit = (e) =>  {
-
- }
- const handleDelete = (e) => {
-
- }
-let reviewUserButtons
-
+function ReviewsDiv() {
+  const { spotId } = useParams();
+  const dispatch = useDispatch();
+  const reviews = useSelector((state) => state.spots[spotId]?.Reviews);
+  const sessionUser = useSelector((state) => state.session.user?.id);
+  const review = useSelector((state) => state?.spots[spotId]?.Review);
+  const handleSubmit = (e) => {};
+  const handleDelete = (e) => {};
+  let reviewUserButtons;
 
   // if (sessionUser && sessionUser.id === review.userId) {
   //   reviewUserButtons = (
@@ -33,41 +28,42 @@ let reviewUserButtons
   //   reviewUserButtons = null;
   // }
 
-let reviewDisplay;
+  let reviewDisplay;
 
-if(!reviews) {
-  reviewDisplay = (
-    <div className="reviewsDiv">No reviews for this spot yet... </div>
-  )
-}else{
-  reviewDisplay = (
-    <div>
-      <ul className="reviewsDiv">
-        {reviews.map((review) => {
-          if (review) {
-            return (
-              <div className="singleReview">
-                <li className="reviewText" key={review.id}>
-                  {review.content}
-                </li><br/>
-                <li className="updatedAt" key={review.updatedAt}>{review.updatedAt}</li>
-                {reviewUserButtons}
-              </div>
-            );
-          }
-        })}
-      </ul>
-    </div>
-  );
-}
+  if (!reviews) {
+    reviewDisplay = (
+      <div className="reviewsDiv">No reviews for this spot yet... </div>
+    );
+  } else {
+    reviewDisplay = (
+      <div>
+        <ul className="reviewsDiv">
+          {reviews.map((review) => {
+            if (review) {
+              return (
+                <div className="singleReview">
+                  <li className="reviewText" key={review.id}>
+                    {review.content}
+                  </li>
+                  <br />
+                  <li className="updatedAt" key={review.updatedAt}>
+                    {review.updatedAt}
+                  </li>
+                  {reviewUserButtons}
+                </div>
+              );
+            }
+          })}
+        </ul>
+      </div>
+    );
+  }
 
   return (
     <>
-    <div>
-      {reviewDisplay}
-    </div>
+      <div>{reviewDisplay}</div>
     </>
-  )
+  );
 }
 
 export default ReviewsDiv;
